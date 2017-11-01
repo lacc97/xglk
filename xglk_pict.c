79844a6bf54607d4f266161a2ec9c3b21d9019af
Luis Cáceres <lacaceres97@gmail.com>
2017-11-01 19:56:25 +0000

    Remove deprecated png_ptr->jmpbuf

--- xglk_pict.c
+++ xglk_pict.c
@@ -385,7 +385,7 @@ static XImage *load_image_png(FILE *fl)
     return NULL;
   }
 
-  if (setjmp(png_ptr->jmpbuf)) {
+  if (setjmp(png_jmpbuf(png_ptr))) {
     /* If we jump here, we had a problem reading the file */
     png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
     if (rowarray)
